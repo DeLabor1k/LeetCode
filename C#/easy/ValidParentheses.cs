f04e6855e4249c43a92787de792324f03304fe81
DeLabor <cotrellyt@gmail.com>
2026-04-02 21:24:49 +0700

    Add files via upload

---
+++ C#/easy/ValidParentheses.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        // Task 20 | Easy
+
+        Stack<char> stack = new Stack<char>();
+        
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0) 
+                    return false;
+
+                    char top = stack.Pop();
+
+                    if (c == ')' && top != '(') return false;
+                    if (c == ']' && top != '[') return false;
+                    if (c == '}' && top != '{') return false;
+                }
+            } return stack.Count == 0;
+    }
+}
