170997abd532e635359c983919c853fa9b4612de
DeLabor <cotrellyt@gmail.com>
2026-04-05 08:57:10 +0700

    Add files via upload

---
+++ C#/easy/PlusOne.cs
@@ -0,0 +1,19 @@
+public class Solution 
+{
+    // Task 66 | Easy
+    public int[] PlusOne(int[] digits) 
+    {
+         for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            } 
+                digits[i] = 0; 
+        }
+            int[] newArray = new int[digits.Length + 1];
+            newArray[0] = 1;
+            return newArray;
+    }   
+}
