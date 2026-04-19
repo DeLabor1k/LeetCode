736025159db508109247013e75e2d04dbe9f52e4
DeLabor <cotrellyt@gmail.com>
2026-04-19 07:44:14 +0700

    Add files via upload

---
+++ C#/hard/RegularExpressionMatching.cs
@@ -0,0 +1,46 @@
+public class Solution 
+{
+    // Task 10 | Hard
+    public bool IsMatch(string s, string p) 
+    {
+        int m = s.Length;
+        int n = p.Length;
+
+        bool[,] dp = new bool[m + 1, n + 1];
+
+        dp[0, 0] = true;
+        
+
+        for (int j = 2; j <= n; j++)
+        {
+            if (p[j - 1] == '*')
+            {
+                 dp[0, j] = dp[0, j - 2];
+            }
+        }
+            for(int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (p[j - 1] == '*')
+                    {
+                         dp[i, j] = dp[i, j - 2];
+
+                        char charBstar = p[j - 2];
+                        if (charBstar == '.' || charBstar == s[i - 1])
+                        {
+                            dp[i, j] = dp[i, j] || dp[i - 1, j];
+                        }
+                    }
+                    else
+                    {
+                        if (p[j - 1] == '.' || p[j - 1] == s[i - 1])
+                        {
+                            dp[i, j] = dp[i - 1, j -1];
+                        }
+                    }
+                }
+            }
+            return dp[m, n];
+    }
+}
