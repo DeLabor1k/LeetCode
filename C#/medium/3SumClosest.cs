8127c39922e0159639cf9d25206a1a092631f379
DeLabor <cotrellyt@gmail.com>
2026-04-25 18:13:13 +0700

    Add files via upload

---
+++ C#/medium/3SumClosest.cs
@@ -0,0 +1,26 @@
+public class Solution 
+{
+    // Task 16 | Medium
+    public int ThreeSumClosest(int[] nums, int target) 
+    {
+        Array.Sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) return sum;
+                if (Math.Abs(sum - target) < Math.Abs(closestSum - target))
+                    closestSum = sum;
+                if (sum < target) left++;
+                else right--;
+            }
+        }
+        return closestSum;
+    }
+}
