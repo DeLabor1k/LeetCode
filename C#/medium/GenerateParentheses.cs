4ef3e8ed953d3d25df7fe7b70d2b76436bf58937
DeLabor <cotrellyt@gmail.com>
2026-04-28 16:50:12 +0700

    Add files via upload

---
+++ C#/medium/GenerateParentheses.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    // Task 22 | Medium
+    public IList<string> GenerateParenthesis(int n) 
+    {
+        List<string> result = new List<string>();
+        Backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    private void Backtrack(List<string> result, string current, int open, int close, int n)
+    {
+        if (current.Length == 2 * n)
+        {
+            result.Add(current);
+            return;
+        }
+    
+        if (open < n)
+            Backtrack(result, current + "(", open + 1, close, n);
+    
+        if (close < open)
+            Backtrack(result, current + ")", open, close + 1, n);
+    }
+}
